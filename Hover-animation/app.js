6ec65e9e528b6d1ee8a9cb2c3394d779f66bf375
umit-turk <76227146+umit-turk@users.noreply.github.com>
2021-04-14 14:24:14 +0300

    Add files via upload

---
+++ Hover-animation/app.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.1,
+    image1: './photo1.jpg',
+    image2: './photo2.jpg',
+/*     angle: Math.PI / 4, */
+    speedIn: 2,
+    speedOut:3,
+    displacementImage: './stripe1.png'
+})
